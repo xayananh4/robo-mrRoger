588fa0aca24f5cf1e734f0ee7e2128a7f5ce0f7a
viradeth xay-ananh <xayananh4@gmail.com>
2023-01-20 14:30:39 -0800

    change beep function name to beeboopRoger

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
-Describe: beep(number)
+Describe: beepBoopRoger(number)
 
-Test: "It should return an array from 0 to the user's inputted number replacing the number 1 with beep"
-Code: beep(5);
+Test: "It should return an array from 0 to the user's inputted number replacing number with 1 to string: beep! "
+Code: beepBoopRoger(5);
 Expected Output: [0, "beep", 2, 3, 4, 5]
 
+Test: "It should return array starting from 0 to user's input number replacing numbers with 2 string: boop!"
+Code: beepBoopRoger(5);
+Expected Output: [0, "beep!", "boop!", 3, 4, 5]
 
-
-Test: "It should..." // this is the start of 2nd test for beepBoop()
+Test: "It should return array starting from 0 to user's input number replacing numbers with 3 to string: Won't you be my neighbor?"
+Code: beepBoopRoger(5);
+Expected Output: [0, "beep!", "boop!", "Won't you be my neighbor?", 4, 5]
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,3 @@
-function isOneInTheArray(number, index) {
-  
-  return [0,1,2,3].includes(number, index);
-}
 function populateArrayFromInput(countToInput){
 let outputArray = [];
 for(i = 0; i <= countToInput; i++) {
@@ -9,12 +5,19 @@ for(i = 0; i <= countToInput; i++) {
 }
 return outputArray;
 }
-function beep(countToInput){
-let newOutputArray = [];
-let arrayPopFromInput = populateArrayFromInput(countToInput);
-arrayPopFromInput.forEach(function(element) {
 
-  if(element.toString().includes(1)) {
+function beepBoopRoger(countToInput){
+let arrayPopFromInput = populateArrayFromInput(countToInput);
+let newOutputArray = [];
+
+arrayPopFromInput.forEach(function(element) {
+   if(element.toString().includes(3)) {
+    newOutputArray.push('"Won\'\t you be my neighbor?"');
+  }
+  else if(element.toString().includes(2)) {
+    newOutputArray.push('"Beep!"');
+  }
+  else if(element.toString().includes(1)) {
     newOutputArray.push('"Beep!"');
   }
   else {
@@ -22,15 +25,13 @@ arrayPopFromInput.forEach(function(element) {
  }
 });
 return newOutputArray;
-
-
 }
 
 function handleFormSubmission(event) {
   event.preventDefault();
   let countToInput = document.getElementById("userInput");
   let paraElement = document.getElementById("results");
-  paraElement.innerText = beep(countToInput.value);
+  paraElement.innerText = beepBoopRoger(countToInput.value);
 }
 
 window.addEventListener("load", function() {
