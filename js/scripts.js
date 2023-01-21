403f7f4fa16a2927234101e16833845aa110f157
viradeth xay-ananh <xayananh4@gmail.com>
2023-01-20 16:22:27 -0800

    add error handling

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// function to populate array 
 function populateArrayFromInput(countToInput){
 let outputArray = [];
 for(i = 0; i <= countToInput; i++) {
@@ -6,36 +7,58 @@ for(i = 0; i <= countToInput; i++) {
 return outputArray;
 }
 
+//business logic
 function beepBoopRoger(countToInput){
 let arrayPopFromInput = populateArrayFromInput(countToInput);
 let newOutputArray = [];
 
-arrayPopFromInput.forEach(function(element) {
-   if(element.toString().includes(3)) {
-    newOutputArray.push('"Won\'\t you be my neighbor?"');
-  }
-  else if(element.toString().includes(2)) {
-    newOutputArray.push('"Beep!"');
-  }
-  else if(element.toString().includes(1)) {
-    newOutputArray.push('"Beep!"');
-  }
-  else {
-    newOutputArray.push(element);
- }
-});
+if(arrayPopFromInput.length !== 0){  
+  arrayPopFromInput.forEach(function(element) {
+    if(element.toString().includes(3)) {
+      newOutputArray.push('"Won\'\t you be my neighbor?"');
+    }
+    else if(element.toString().includes(2)) {
+      newOutputArray.push('"Boop!"');
+    }
+    else if(element.toString().includes(1)) {
+      newOutputArray.push('"Beep!"');
+    }
+    else {
+      newOutputArray.push(element);
+    }
+  });
 return newOutputArray;
 }
+else {
+  return null;
+}
+}
 
-function handleFormSubmission(event) {
+//UI logic
+function handleFormSubmission(event) {  
   event.preventDefault();
   let countToInput = document.getElementById("userInput");
+  let beepBoopRogerResults = beepBoopRoger(countToInput.value);
   let paraElement = document.getElementById("results");
-  paraElement.innerText = beepBoopRoger(countToInput.value);
+  let resultsFromUser = document.querySelector("div#resultsFromUser");
+  
+ 
+  
+  if(beepBoopRogerResults)
+  {
+    resultsFromUser.innerText = beepBoopRogerResults;
+  }
+  else
+  {
+    resultsFromUser.setAttribute("class", "defaultSettingHidden");
+    document.querySelector("div#error-passage").innerText = "Please enter numbers 0 or higher";
+  }
+
+  
+  
+  
 }
 
 window.addEventListener("load", function() {
   document.querySelector("form#input-form").addEventListener("submit", handleFormSubmission);
-
-
 });
