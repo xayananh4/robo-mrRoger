fee087e4e1f7cc5e95c329008228887a2aa76d11
xayananh4 <xayananh4@gmail.com>
2023-01-20 21:48:42 -0800

    add reverse results function

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,28 @@ function populateArrayFromInput(countToInput) {
 }
 
 //business logic
+function reverseArray(_array) {
+    // let arrayPopFromInput = populateArrayFromInput(countToInput);
+    // let original_array = populateArrayFromInput(countToInput);
+
+    let reversed_array = [];
+
+    for (let i = _array.length - 1; i >= 0; i--) {
+        
+        reversed_array.push(original_array[i]);
+    }
+    console.log(reversed_array);
+    return reversed_array.join(" ");
+
+}
+
+//business logic
 function beepBoopRoger(countToInput) {
   let arrayPopFromInput = populateArrayFromInput(countToInput);
   let newOutputArray = [];
 
   if (arrayPopFromInput.length !== 0) {
+    
     arrayPopFromInput.forEach(function (element) {
       if (element.toString().includes(3)) {
         newOutputArray.push('"Won\'t you be my neighbor?"');
@@ -29,16 +46,28 @@ function beepBoopRoger(countToInput) {
     });
     return newOutputArray.join(" ");
   }
+  
   else {
     return null;
   }
 }
 
 //UI logic
+function reserverFormSubmission() {
+  let countToInput = document.getElementById("userInput");
+  let reverseResults = reverseArray(countToInput.value);
+  let reverseResultsEelement = document.querySelector("div#reversed");
+
+
+
+    reverseResultsEelement.innerText = reverseResults ;
+
+}
+
+//UI logic
 function handleFormSubmission() {
   let countToInput = document.getElementById("userInput");
   let beepBoopRogerResults = beepBoopRoger(countToInput.value);
-  let paraElement = document.getElementById("results");
   let resultsFromUser = document.querySelector("div#resultsFromUser");
   let errorPassage = document.querySelector("div#error-passage");
 
@@ -55,9 +84,25 @@ function handleFormSubmission() {
 }
 
 window.addEventListener("load", function () {
+  let reverseBtn = document.querySelector("button#reverse");
+  let countToInput = document.getElementById("userInput");
+  
 
   document.querySelector("form#input-form").addEventListener("submit", function (event) {
     handleFormSubmission();
     event.preventDefault();
   });
+
+  // New event listener for after form submit event the reverse button is shown
+  document.querySelector("form#input-form").addEventListener("submit", function () {
+    reverseBtn.removeAttribute("class");
+  });
+
+  // Event listener to reverse values when user clicks reverse button
+  reverseBtn.addEventListener("click", function () {
+
+ 
+  });
+
+
 });
